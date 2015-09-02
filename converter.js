673340e654dfd98987bc0b9531ae662e8ec2fba1
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2015-09-02 12:51:40 +0430

    input method changed from file copying to source inserting

--- converter.js
+++ converter.js
@@ -1,27 +1,76 @@
 
 
 (function () {
+  var ui_delay_ms = 500;
 
-
-  var initialKeys = Object.keys(window);
-
-  loadScript('test').then(function () {
-    var newKeys = Object.keys(window);
-    var diffKeys = getDiff(initialKeys, newKeys);
-    console.log("Diffs are");
-    console.log(diffKeys);
+  $('#detectApi').click(function () {
+    resetView();
+    var newKeys = detectApi();
+    transitionToChooseAPI();
+    renderApiStep(newKeys);
   });
 
+  function renderApiStep (newKeys) {
+    var tmpl = $('#api-methods-tmpl').html();
+    $('#api-methods').empty();
 
-  function loadScript (srcPath) {
-    console.log("loding " + srcPath);
-    var scriptLoadingDfd = $.Deferred();
-
-    require(["inputScripts/" + srcPath], function (test) {
-      scriptLoadingDfd.resolve();
+    newKeys.forEach(function (method) {
+      var node = tmpl.replace(/\{\{methodName\}\}/g, method);
+      $('#api-methods').append($(node));
     });
+  }
 
-    return scriptLoadingDfd.promise();
+  function transitionToChooseAPI () {
+    var $progress = $('#step-input-progress');
+    $progress.removeClass('hidden');
+    setTimeout(function () {
+      $progress.addClass('hidden');
+      $('#step-api').fadeIn();
+    }, ui_delay_ms);
+  }
+
+  function resetView () {
+    $('#step-api, #step-output').hide();
+  }
+
+  function detectApi () {
+    var initialKeys = Object.keys(window);
+    var inputSource = $('#source').val();
+    try {
+      eval.call(window, inputSource);
+    } catch (err) {
+      alert('The script you inserted have errors. Please make sure your code is clean before you hit the detect button.');
+      throw(err);
+    }
+    var newKeys = Object.keys(window);
+    var diffKeys = getDiff(initialKeys, newKeys);
+    return diffKeys;
+  }
+
+  function Template (source) {
+    this.src = source;
+    this.render = function () {
+      Array.prototype.forEach.call(arguments, function (placeholder) {
+        console.log(placeholder);
+      });
+      return 'string';
+    };
+    return this;
+  }
+
+  function convertToAMD (body, deps, api) {
+    var source = 'define([{{deps}}], function ({{names}}) {\n' +
+      '{{body}}\n' +
+      'return {\n' +
+        '{{api}}\n' +
+      '}\n' +
+    '});\n';
+
+    var t = new Template(source);
+    return t.render({
+      deps: deps,
+      api: api
+    });
   }
 
   function getDiff (origArr, newArr) {
